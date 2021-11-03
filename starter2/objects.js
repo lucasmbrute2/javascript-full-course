1c56be7862c90234c4b2b6e65c7719477f5bf87f
Lucas <lucasmbrute614@gmail.com>
2021-11-03 13:47:52 -0300

    strict string

--- starter2/objects.js
+++ starter2/objects.js
@@ -1,3 +1,4 @@
+"use strict"
 // 1. Create an object called 'myCountry' for a country of your choice, containing 
 // properties 'country', 'capital', 'language', 'population' and 
 // 'neighbours' (an array like we used in previous assignments
