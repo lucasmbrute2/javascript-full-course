4ee2cc5a522c98f303f4b1651bf6b14de1bf41de
Lucas <lucasmbrute614@gmail.com>
2021-12-21 01:01:51 -0300

    94->studying about hoisting

--- starter4/script.js
+++ starter4/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// STUDYING SCOPE
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -18,3 +19,26 @@ function calcAge(birthYear) {
 
 const firstName = 'Lucas';
 calcAge(1994);
+
+/* STUDYING HOW HOISTING WORKS
+
+-> First, talking about functions, there are function EXPRESSIONS, they are =>*/
+
+const declaration = function () {
+  return true;
+};
+//-> Function DECLARATION
+function Expression() {
+  return true;
+}
+
+//-> ARROW FUNCTION
+const declaration = () => {
+  return true;
+};
+
+// Hoisting make some kind of variable acessible in the code, in fact, before were declared
+
+// FUNCTION DECLARATIONS are Hoistable, VAR variables either.
+
+//Function Expressions and Arrows
