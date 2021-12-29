a5c7fe9ec52a2318219b5739f142f5fac46009cf
Lucas <lucasmbrute614@gmail.com>
2021-12-29 00:10:09 -0300

    100 => Resolving the problem with Object.asign making  a shallow copy.

--- starter4/primitive-vs-objects.js
+++ starter4/primitive-vs-objects.js
@@ -43,3 +43,16 @@ console.log('Before marriage:', jessica);
 console.log('After marriage:', marriedJessica);
 
 //Making this work with Reference values
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const jessicaCopy = Object.assign({}, jessica2); // This method merge 2 objects and then return a new one.
+jessicaCopy.lastName = 'Davis';
+console.log(jessicaCopy, jessica2);
+
+//jessicaCopy is now a copy of jessica2, a new object was in fact created in the Heap, and jessicaCopy is now poiting to that object.
+
+// THE PROBLEM IS SOLVED BUT... there is a one problem. The object assign only works in the first level. Its a shallow copy, whats mean, if we have an object inside the object, this inner object will still be the same. Both the variables are be changed.
