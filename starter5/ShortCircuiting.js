2c96fa8136a09c6c08a945452b4da4700af1c0c0
lucasmbrute2 <lucasmbrute614@gmail.com>
2022-01-08 16:12:03 -0300

    107 => Ternary with short circuiting

---
+++ starter5/ShortCircuiting.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainMenuIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainMenuIndex]];
+  },
+
+  orderDelivery: function ({
+    time = '10:15',
+    address = 'Rua David Lopes',
+    mainIndex,
+    starterIndex,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be devilered to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log({ mainIngredient });
+    console.log({ otherIngredients });
+  },
+};
+
+console.log(3 || 'Lucas');
+console.log('' || 'Lucas');
+console.log(true || 0);
+console.log(undefined || null); // This will return null
+
+//Explanation
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); //This will return ''Hello'' cuz JS returns the first truthy value.
+
+// restaurant.numGuest = 'exists';
+const guests1 = restaurant.numGuest ? restaurant.numGuest : 'dont exists';
+console.log(guests1);
+
+//The other way to do this:
+
+restaurant.numGuest = 'exists';
+const guests2 = restaurant.numGuest || 'dont exists';
+console.log(guests2);
