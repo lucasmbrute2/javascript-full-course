fe23fa11f0c2883a812d6fbf43311f9e68c2438e
lucasmbrute2 <lucasmbrute614@gmail.com>
2022-01-17 10:49:14 -0300

    113=> Option chaining sintaxe

--- starter5/optionChaining.js
+++ starter5/optionChaining.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    [`day-${2 + 4}`]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order(starterIndex, mainMenuIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainMenuIndex]];
+  },
+
+  orderDelivery({
+    time = '10:15',
+    address = 'Rua David Lopes',
+    mainIndex,
+    starterIndex,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be devilered to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log({ mainIngredient });
+    console.log({ otherIngredients });
+    console.log('====================================');
+  },
+};
+
+//The optional channing cames to resolve this problem:
+console.log(restaurant.openingHours);
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// WITH OPTION CHAINING
+
+console.log(restaurant.openingHours?.mom?.open);
