8567df2e072ff270dc45272cb0e4315f57e497ff
lucasmbrute2 <lucasmbrute614@gmail.com>
2022-01-26 22:16:25 -0300

    117=> How maps works

--- starter5/maps.js
+++ starter5/maps.js
@@ -1,1 +1,72 @@
 'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    [`day-${2 + 4}`]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+    [weekdays[2]]: {
+      open: 11,
+      colse: 20,
+    },
+  },
+
+  order(starterIndex, mainMenuIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainMenuIndex]];
+  },
+
+  orderDelivery({
+    time = '10:15',
+    address = 'Rua David Lopes',
+    mainIndex,
+    starterIndex,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be devilered to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log({ mainIngredient });
+    console.log({ otherIngredients });
+    console.log('====================================');
+  },
+};
+
+const rest = new Map();
+
+// Key maps can be anything
+
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // The map will return updated
+
+//Chaining set
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set(true, 'value')
+  .set(new Map(), 'novo map');
+console.log(rest);
