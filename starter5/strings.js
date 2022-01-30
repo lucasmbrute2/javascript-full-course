c1a8acf3e386dc5d5c59b369b58f14a386cde644
lucasmbrute2 <lucasmbrute614@gmail.com>
2022-01-30 12:35:24 -0300

    Update strings.js

--- starter5/strings.js
+++ starter5/strings.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+//String are iterable
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]); //Getting imediatly the position
+
+console.log(plane.length); //You can get the o length of that.
+console.log('B737'.length);
+
+//======METHODS======
+
+//IndexOf
+console.log(airline.indexOf('r')); //Getting the position of the letter in the string.
+console.log(airline.lastIndexOf('r')); //Getting the last ocurrency of the letter in the string
+console.log(airline.indexOf('Portugal')); // This will return 8 because the String Portugal starts in the index 8
