d4b402d90f069417961eb0fbc53b53a6df9c9437
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-01-31 22:55:00 -0300

    136=> Immediately Invoked Function Expression

--- starter6/script.js
+++ starter6/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce();
+
+//Making a function be called only once
+
+(function () {
+  console.log('THIS will never run again');
+})(); //Immediately Invoked Function Expression
+
+//Arrow Function
+(() => console.log('THIS also will never run again'))();
