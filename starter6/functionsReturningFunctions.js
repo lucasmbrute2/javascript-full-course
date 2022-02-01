fed915a158ee3302ff8bfaf451fdf52ee1d1661f
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-01 15:34:40 -0300

    132=> Functions returning another function syntax

--- starter6/functionsReturningFunctions.js
+++ starter6/functionsReturningFunctions.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greetStore = greet('Hey'); //The greetStore is the function inside the greet
+greetStore('Lucas');
+
+//ANOTHER SYNTAX
+greet('Hello')('Matheus');
+
+//Rewriting the first function using ARROW FUNCTION
+
+// const greet2 = greeting => {
+//   return name => {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+const greet2 = greeting => name => console.log(`${greeting} ${name}`);
+greet2('Whats up')('Carol');
