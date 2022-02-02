faf30a13344a967bccd8f8ef58db056da1e501a8
lucasmbrute2 <97185277+lucas-victor-dantas@users.noreply.github.com>
2022-02-02 20:56:08 -0300

    134=> Starting with BIND

--- starter6/bind.js
+++ starter6/bind.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const gol = {
+  airline: 'Gol',
+  iataCode: 'GL',
+  bookings: [],
+  // book: function(){},
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode} ${flightNum}`, name });
+  },
+};
+
+const euroWings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+const book = gol.book;
+book.call(euroWings, 203, 'Lucas'); //Paste example
+//**BIND
+const bookEW = book.bind(euroWings); // differently of CALL and APPLY method, bind create a new function, he doest not call the 'book' function, instead return a new one.
+const bookLX = book.bind(swiss);
+const bookGL = book.bind(gol);
+
+bookEW(45, 'Lucas Bind');
