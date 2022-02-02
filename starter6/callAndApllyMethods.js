d9ce220d7b6b5e2ce707c165b6cc84dedea0812b
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-01 23:30:40 -0300

    Explanation about THIS

--- starter6/callAndApllyMethods.js
+++ starter6/callAndApllyMethods.js
@@ -1,5 +1,10 @@
 'use strict';
+/*THIS keyword in a function needs to be an method, otherwise its will return undefined.
+*In ARROW FUNCTIONS the THIS keyword returns the parent THIS.
+*In a regular function returns undefined.
+*In a method returns the object(owner)
 
+*/
 const gol = {
   airline: 'Gol',
   iataCode: 'GL',
@@ -9,8 +14,18 @@ const gol = {
     console.log(
       `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
     );
+    this.bookings.push({ flight: `${this.iataCode} ${flightNum}`, name });
   },
 };
+console.log(gol);
 
 gol.book(239, 'Lucas Souza');
 gol.book(635, 'Lucas Victor');
+
+const euroWings = {
+  name: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = gol.book;
