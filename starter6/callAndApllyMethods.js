0752dbc347de46262e6f15182df45ca921bc924e
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-01 23:41:28 -0300

    133=> Method of function CALL

--- starter6/callAndApllyMethods.js
+++ starter6/callAndApllyMethods.js
@@ -23,9 +23,17 @@ gol.book(239, 'Lucas Souza');
 gol.book(635, 'Lucas Victor');
 
 const euroWings = {
-  name: 'Eurowings',
+  airline: 'Eurowings',
   iataCode: 'EW',
   bookings: [],
 };
 
-const book = gol.book;
+const book = gol.book; //This is the new function "book"
+
+/*
+Returns undefinez cuz its a regular function now, not a method.
+book(23, 'Bruna Silva');
+
+So, instead of just doing this, we can make THIS:
+*/
+book.call(euroWings, 23, 'Bruna Silva');
