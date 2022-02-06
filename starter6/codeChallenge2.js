ee2f42aa74daef32f1d84ad748c98d54629521ac
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-06 14:10:07 -0300

    139=> Challenge done

--- starter6/codeChallenge2.js
+++ starter6/codeChallenge2.js
@@ -14,4 +14,8 @@
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 })();
