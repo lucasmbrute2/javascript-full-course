249d8c9004361b4ec9a2d338888c66be6bee934c
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-11 11:32:46 -0300

    154=> Resolution

--- starter7/codeChallenge2.js
+++ starter7/codeChallenge2.js
@@ -20,4 +20,28 @@
 
 // Test data:
 // § Data 1: [5, 2, 4, 1, 15, 8, 3]
-// § Data 2: [16, 6, 10, 5, 6, 1, 4
+// § Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+function calcAverageHumanAge(ages) {
+  const convertToHumanAge = ages.map(age =>
+    age <= 2 ? age * 2 : 16 + age * 4
+  );
+
+  const fiterDogsLess18 = convertToHumanAge.filter(dogAge => dogAge >= 18);
+
+  //   const averageOfDogsYear = (
+  //     fiterDogsLess18.reduce((acc, cur, index, arr) => {
+  //       return cur + acc;
+  //     }) / fiterDogsLess18.length
+  //   ).toFixed(2);
+
+  const averageOfDogsYear = fiterDogsLess18
+    .reduce((acc, cur, index, arr) => acc + cur / arr.length, 0)
+    .toFixed(2);
+  return averageOfDogsYear;
+}
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
