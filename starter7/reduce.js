0dba43b774aab2ceae794754dcab64f24834bae8
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-10 23:25:21 -0300

    153=> Reduce method vs For of loop

--- starter7/reduce.js
+++ starter7/reduce.js
@@ -57,3 +57,16 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//REDUCE
+//acc = SNOWBALL, the element that holds everything.
+//cur = the currently element.
+const balance = movements.reduce((acc, cur) => (acc += cur), 0);
+console.log(balance);
+
+//For of loop
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+
+console.log(balance2);
