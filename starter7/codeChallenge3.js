e00d5c22255c5af50e90d3f844e7d137062b2557
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-11 22:15:57 -0300

    156=> Coding challenge 3

---
+++ starter7/codeChallenge3.js
@@ -0,0 +1,25 @@
+// Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+// as an arrow function, and using chaining!
+
+// function calcAverageHumanAge(ages) {
+//     const convertToHumanAge = ages.map(age =>
+//       age <= 2 ? age * 2 : 16 + age * 4
+//     );
+
+//     const fiterDogsLess18 = convertToHumanAge.filter(dogAge => dogAge >= 18);
+
+//     //   const averageOfDogsYear = (
+//     //     fiterDogsLess18.reduce((acc, cur, index, arr) => {
+//     //       return cur + acc;
+//     //     }) / fiterDogsLess18.length
+//     //   ).toFixed(2);
+
+//     const averageOfDogsYear = fiterDogsLess18
+//       .reduce((acc, cur, index, arr) => acc + cur / arr.length, 0)
+//       .toFixed(2);
+//     return averageOfDogsYear;
+//   }
+
+// Test data:
+// § Data 1: [5, 2, 4, 1, 15, 8, 3]
+// § Data 2: [16, 6, 10, 5, 6, 1, 4
