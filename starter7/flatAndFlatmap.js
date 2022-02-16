4872128422d47cd6e51f4c4fdfca2bad3603f0aa
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-16 10:28:40 -0300

    162=> Deeply flat method

--- starter7/flatAndFlatmap.js
+++ starter7/flatAndFlatmap.js
@@ -8,3 +8,12 @@ const arr = [
 
 const flatedArr = arr.flat();
 console.log(flatedArr); //Remove the nested array and creates a big one
+
+const arrDeep = [
+  [[200, 450], -400],
+  [[3000, -650], -130],
+  [70, 1300],
+];
+
+console.log(arrDeep.flat()); //Notice that Flat only works for 1 level of nesting.
+console.log(arrDeep.flat(2)); //But we can pass in the arguments the number of deeping
