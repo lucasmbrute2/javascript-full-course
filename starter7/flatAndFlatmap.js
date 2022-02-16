3a69f71a55b25ccf34dce18bb80b11f029d457eb
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-16 10:25:04 -0300

    162=> Flat method

--- starter7/flatAndFlatmap.js
+++ starter7/flatAndFlatmap.js
@@ -1,3 +1,10 @@
 'use strict';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const arr = [
+  [200, 450, -400],
+  [3000, -650, -130],
+  [70, 1300],
+];
+
+const flatedArr = arr.flat();
+console.log(flatedArr); //Remove the nested array and creates a big one
