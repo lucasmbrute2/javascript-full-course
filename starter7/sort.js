45d7bf0752ec75a34dff6a26cdad557f40ea9a26
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-17 01:29:10 -0300

    163=> Descendant order

--- starter7/sort.js
+++ starter7/sort.js
@@ -7,11 +7,16 @@ const owners = ['Lucas', 'Jonas', 'Adam', 'Martha'];
 console.log(owners.sort());
 
 //**NUMBERS
-//return < 0 A,B
-//return > 0 B, A
+//return < 0 A,B (keep order)
+//return > 0 B, A (switch order)
 movements.sort((a, b) => {
   if (a > b) return 1;
   else if (b > a) return -1;
 });
 
+//Descendant
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  else if (b > a) return 1;
+});
 console.log(movements);
