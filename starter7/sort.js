2b391b31afd00dfcbe219c628ca0fc1c3c10093a
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-17 01:26:49 -0300

    163=> Diference between sort Strings and Numbers

--- starter7/sort.js
+++ starter7/sort.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const owners = ['Lucas', 'Jonas', 'Adam', 'Martha'];
+
+//**STRINGS
+console.log(owners.sort());
+
+//**NUMBERS
+//return < 0 A,B
+//return > 0 B, A
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  else if (b > a) return -1;
+});
+
+console.log(movements);
