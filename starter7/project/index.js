fd2e9200b68af41dbfc2be6325f450e5a07e005e
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-17 01:40:30 -0300

    163=> Refactoring the code to sort the array of movements

--- starter7/project/index.js
+++ starter7/project/index.js
@@ -56,10 +56,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach((mov, index) => {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, index) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -267,3 +269,6 @@ function countingAllMovements2(accounts) {
   return accountMovements;
 }
 console.log(countingAllMovements2(accounts));
+
+//Sorting the movementes
+btnSort.addEventListener('click', () => {});
