4c3bbd132bd67ea3027ec16bbaf2c4771491254c
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-17 01:32:54 -0300

    164=> Better way to sort arrays

--- starter7/sort.js
+++ starter7/sort.js
@@ -9,14 +9,18 @@ console.log(owners.sort());
 //**NUMBERS
 //return < 0 A,B (keep order)
 //return > 0 B, A (switch order)
-movements.sort((a, b) => {
-  if (a > b) return 1;
-  else if (b > a) return -1;
-});
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   else if (b > a) return -1;
+// });
+movements.sort((a, b) => a - b); //Better way
+
+console.log(movements);
 
 //Descendant
-movements.sort((a, b) => {
-  if (a > b) return -1;
-  else if (b > a) return 1;
-});
-console.log(movements);
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   else if (b > a) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements); //Better way
