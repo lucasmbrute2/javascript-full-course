38fb16f81dcae04d759ecaab4d233cb793668fbc
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-18 21:38:36 -0300

    164=> Fill method

--- starter7/moreWaysOfCreatingAndFillingArrays.js
+++ starter7/moreWaysOfCreatingAndFillingArrays.js
@@ -5,4 +5,9 @@ const arr = [1,2,3,4,5,6,7]
 const instaArr = new Array(1,2,3,4,5,6,7)
 
 //Building programattly arrays
-console.log(new Array(8))// This wil return a empty array with length 8, cuz we just pass 1 argument at Array class
+const x = new Array(8)
+console.log(x)// This wil return a empty array with length 8, cuz we just pass 1 argument at Array class
+
+//So, what is it for ? 
+x.fill(3)
+console.log(x)//Notice that entire array was filled with this method.
