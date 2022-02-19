ddb699f3e2d3273221046c1940fb3f2b3588f01a
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-18 21:33:31 -0300

    164=> Creating an empty array with especific length

--- starter7/moreWaysOfCreatingAndFillingArrays.js
+++ starter7/moreWaysOfCreatingAndFillingArrays.js
@@ -1,2 +1,8 @@
 'use strict'
 
+//Normal ways to build arrays
+const arr = [1,2,3,4,5,6,7]
+const instaArr = new Array(1,2,3,4,5,6,7)
+
+//Building programattly arrays
+console.log(new Array(8))// This wil return a empty array with length 8, cuz we just pass 1 argument at Array class
