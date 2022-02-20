d497c7b462db1f5ac2ffa795e8cfb6f3ae10ec96
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-20 13:46:23 -0300

    Exercise 1

--- starter7/codeChallenge4.js
+++ starter7/codeChallenge4.js
@@ -37,3 +37,30 @@
 // 8. Create a shallow copy of the 'dogs' array and sort it by recommended food
 // portion in an ascending order (keep in mind that the portions are inside the
 // array's objects �
+
+// Hints:
+// § Use many different tools to solve these challenges, you can use the summary
+// lecture to choose between them �
+// § Being within a range 10% above and below the recommended portion means:
+// current > (recommended * 0.90) && current < (recommended *
+// 1.10). Basically, the current portion should be between 90% and 110% of the
+// recommended portion
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//1.
+(function addingRecommendedFood(dogs) {
+  dogs.forEach(
+    element =>
+      (element.recommendedFood = (element.weight ** 0.75 * 28).toFixed(2))
+  );
+  console.log(dogs);
+  return dogs;
+})(dogs);
+
+console.log(dogs);
