bef15e79417aca0dc9a9805e13b8190ef736f051
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-27 20:58:53 -0300

    189=>Removing event listener

--- 13-Advanced-DOM-Bankist/TypesOfEventsAndEventHandlers/TypesOfEventsAndEventHandlers.js
+++ 13-Advanced-DOM-Bankist/TypesOfEventsAndEventHandlers/TypesOfEventsAndEventHandlers.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = () => {
+  alert('Olá');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1); // Allows multiple EventListener in the same event.
+
+// Bad Practice
+
+// h1.onmouseenter = () => {
+//   alert('Olá2');
+// }; // DON'T USE THAT
