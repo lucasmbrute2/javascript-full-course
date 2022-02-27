af76b5def847072a590509ae309460ea4e812f06
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-27 01:25:50 -0300

    186=>Creating and Inserting elements in DOM

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -9,7 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-  e.preventDefault()
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -19,7 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn=> btn.addEventListener('click',openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,15 +30,41 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
 
-console.log(document.documentElement)
-console.log(document.head)
-console.log(document.body)
+const header = document.querySelector('.header');
 
-document.querySelector('.section')
-const allSections = document.querySelectorAll('.section')// Returns a NodeList, is not automatically updated 
-console.log(allSections)
+document.querySelector('.section');
+const allSections = document.querySelectorAll('.section'); // Returns a NodeList, is not automatically updated
+console.log(allSections);
 
-document.getElementById('section--1')
-const buttons = document.getElementsByTagName('button') // Returns a HTML Collection -> Is automatically updated
-console.log(buttons)
+document.getElementById('section--1');
+const buttons = document.getElementsByTagName('button'); // Returns a HTML Collection -> Is automatically updated
+console.log(buttons);
+
+console.log(document.getElementsByClassName('btn')); // Returns a HTML Colletion
+
+// Creating and Inserting elements
+// .intertAdjacentHTML for example
+
+const message = document.createElement('div'); // We are creating a div, that is not in our DOM yet.
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics';
+
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); // Insert before the element
+header.append(message); // Insert after the element
+// header.append(message.cloneNode(true)); // Copy an element
+
+header.before(message);
+header.after(message);
+
+// Delete Elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
