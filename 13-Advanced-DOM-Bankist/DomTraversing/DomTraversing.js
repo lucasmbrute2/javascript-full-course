038f4a93a589c355c7b4d9f1a56fdac7689f3aa5
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-28 15:28:53 -0300

    193=>Closest VS querySelector

--- 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
+++ 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
@@ -8,3 +8,15 @@ console.log(h1.childNodes); // Returns all the node's child
 console.log(h1.children); // Returns all the element's child in a HTML Colletcion (only direct children)
 h1.firstElementChild.style.color = 'white';
 h1.lastElementChild.style.color = 'tomato';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-primary)'; // Matching the closest element no matter how far away it is in DOM Tree
+
+/* 
+    querySelector vs closest
+
+    'qurySelector' finds the first child, no matter how deep while 'closest' finds the first parent, no matter how deep
+*/
