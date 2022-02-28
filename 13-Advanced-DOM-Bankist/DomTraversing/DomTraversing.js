6b48a02ba09010ffb109875bde07ab62fe98fa40
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-28 03:23:30 -0300

    193=>Going downwards : child

--- 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
+++ 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const h1 = document.querySelector('h1');
+
+// Going downwards : child
+console.log(h1.querySelectorAll('.highlight')); // You can select any deeper level of child elements
+console.log(h1.childNodes); // Returns all the node's child
+console.log(h1.children); // Returns all the element's child in a HTML Colletcion (only direct children)
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'tomato';
