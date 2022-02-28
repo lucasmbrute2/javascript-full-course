e773c23c9119ba68fb33fa4c3b52236acc36a6fe
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-28 15:37:08 -0300

    193=>Finding all elements sibling and applying style

--- 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
+++ 13-Advanced-DOM-Bankist/DomTraversing/DomTraversing.js
@@ -20,3 +20,18 @@ h1.closest('.header').style.background = 'var(--gradient-primary)'; // Matching 
 
     'qurySelector' finds the first child, no matter how deep while 'closest' finds the first parent, no matter how deep
 */
+
+// Going sideays: siblings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+// To find all the siblings
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(element => {
+  if (element !== h1) element.style.transform = 'scale(0.5)';
+}); // Applying this style for all siblings (no for itself)
