8d6429262138b8b8afdd8763cdce9be50c4d800d
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-02-27 21:12:10 -0300

    190=>Bubble phase

--- 13-Advanced-DOM-Bankist/EventPropagation/EventPropagation.js
+++ 13-Advanced-DOM-Bankist/EventPropagation/EventPropagation.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Bubbling Phase
+
+// In the bubbling phase the event will go through all the elements, in this case, from the Button to the document!
+
+const body = document.body;
+const button = document.querySelector('.nav__link--btn');
+
+body.addEventListener('click', () => {
+  alert('Clicou no body');
+});
+
+button.addEventListener('click', () => {
+  alert('Clicou no botão');
+});
+
+// Notice that when we click in button, the body can listen the event
