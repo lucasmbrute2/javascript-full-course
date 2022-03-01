aaf6a71d2d810fbeded96aac06d7e1da7b14f020
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-01 20:10:03 -0300

    202=>DOM content Load event

--- 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
+++ 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
@@ -1,1 +1,10 @@
 'use strict';
+
+/* DOM content loaded
+
+This event occurs when the user enters in the page. Ends when all the HTML and JS is downloaded (exepect images and external things)
+
+*/
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build!', e);
+});
