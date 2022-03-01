9b0df3a1a6ff2e1de71b5e1e5e41bb2e516dd4b1
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-01 02:12:03 -0300

    197 =>A Better Way: The Intersection Observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -144,9 +144,27 @@ nav.addEventListener('mouseout', handleEachEvent.bind(1));
 
 // Sticky navigation
 
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// Sticky navigation with Intersection Observer API
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+  console.log(entry);
+};
+
+const header = document.querySelector('.header');
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
 });
+
+headerObserver.observe(header);
