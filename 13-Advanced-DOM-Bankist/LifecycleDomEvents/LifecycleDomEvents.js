4d9b377b61033ad0f4d0b89b2d30504559dac062
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-01 20:19:11 -0300

    202=>Before unload event

--- 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
+++ 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* DOM content loaded
+/* ==========DOM content loaded===================
 
 This event occurs when the user enters in the page. Ends when all the HTML and JS is downloaded (exepect images and external things)
 
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function (e) {
   console.log('HTML parsed and DOM tree build!', e);
 });
 
-/* Load
+/* ======================Load======================
 
 This event occurs when ALL the content is loaded!
 
@@ -17,3 +17,15 @@ This event occurs when ALL the content is loaded!
 window.addEventListener('load', function (e) {
   console.log('Page fully loaded', e);
 });
+
+/*=================Before unload===============
+
+This event occurs before the user leave the page!
+
+*/
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = ''; // Asking before the user leave the site
+}); // DON'T ABUSE THAT, ONLY USES IF USER WILL LOSES DATA (like writing an email and exits for accident the page.)
