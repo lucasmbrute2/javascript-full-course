cee2b6359de4146e7b74fa41d876a2a5edeb6003
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-01 20:13:30 -0300

    202=>Load event

--- 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
+++ 13-Advanced-DOM-Bankist/LifecycleDomEvents/LifecycleDomEvents.js
@@ -8,3 +8,12 @@ This event occurs when the user enters in the page. Ends when all the HTML and J
 document.addEventListener('DOMContentLoaded', function (e) {
   console.log('HTML parsed and DOM tree build!', e);
 });
+
+/* Load
+
+This event occurs when ALL the content is loaded!
+
+*/
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
