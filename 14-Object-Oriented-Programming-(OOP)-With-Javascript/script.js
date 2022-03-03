1c6667108cf4de32f166913d78c1e5fad89a99fb
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-03 01:57:33 -0300

    208=> Constructor Function (how works)

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/script.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const Person = function (firstName, borthYear) {};
+
+new Person('Lucas', 2000);
+
+//====== What is happining in this function(because the new operator) ==========
+
+// 1. New empty object is created
+// 2. Function is called, this = is the new created object
+// 3. The new object is linked to prototype
+// 4. Function is automatically returned
