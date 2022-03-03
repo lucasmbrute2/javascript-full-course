a1942d78a0c3a57908e4bb9b660f0181fba18d65
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-03 02:09:32 -0300

    208=>Constructor Function tips

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/script.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/script.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+  // Instace properties
+
   this.firstName = firstName; // We are setting the key firstName(could be anything) with the value firstName
   this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 const lucas = new Person('Lucas', 2000);
 console.log(lucas);
@@ -13,3 +20,9 @@ console.log(lucas);
 // 2. Function is called, this = is the new created object
 // 3. The new object is linked to prototype
 // 4. Function is automatically returned
+
+const matilda = new Person('Matilda', 2017);
+const bruna = new Person('Bruna', 2003);
+console.log(matilda, bruna);
+
+console.log(lucas instanceof Person);
