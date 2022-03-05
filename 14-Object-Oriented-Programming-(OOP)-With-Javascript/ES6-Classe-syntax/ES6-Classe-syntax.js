23098a6d003d1dea9679fa9a4cfd10f35413646e
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-05 03:18:42 -0300

    213=> ES6 Syntax classes vs older syntax classes

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/ES6-Classe-syntax/ES6-Classe-syntax.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/ES6-Classe-syntax/ES6-Classe-syntax.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// OLDER SYNTAX COMPARE
+
+function Person(firstName, birthYear) {
+  this.name = firstName;
+  this.firstName = birthYear;
+}
+
+const person1 = new Person('Amanda', 2000);
+console.log(person1);
+
+Person.prototype.calcAge = function () {
+  return 2037 - this.birthYear;
+};
+
+// ================NEW SYNTAX=================
+/* Class expression
+
+const PersonCL = class{}
+
+*/
+// Class declaration
+
+class PersonCL {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return 2037 - this.birthYear; // In ES6 Classes we can create the methods directly here, they aren't attached to the objects themselves, but to the prototype.
+  }
+}
+
+const lucas = new PersonCL('lucas', 2000);
+console.log(lucas.calcAge());
