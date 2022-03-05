5d7dabb64494732d9be7bea7b17b5667f13c7fe7
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-05 04:54:17 -0300

    215=>Static method explanation

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/StaticMethods/StaticMethods.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/StaticMethods/StaticMethods.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// Static method mean that method is available only in the constructor and don't inherits to prototype
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  static greet() {
+    console.log(`Hello Lucas`);
+  }
+}
+
+const lucas = new PersonCl('Lucas', 2000);
+console.log(lucas.__proto__); // The method greet isn't available here
+
+PersonCl.greet();
+console.dir(PersonCl); // It's only available here
