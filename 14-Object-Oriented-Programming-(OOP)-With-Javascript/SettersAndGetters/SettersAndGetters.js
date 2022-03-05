d10472193c6d2a375fb7e0ba6d5411887f880f52
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-05 03:51:32 -0300

    214=>  Getters and Setters working even with Classes

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/SettersAndGetters/SettersAndGetters.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/SettersAndGetters/SettersAndGetters.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+// GETTER
+console.log(account.latest);
+
+// SETTER
+account.latest = 30;
+console.log(account.movements);
+
+// Classes alery have GETTERS AND SETTERS
+
+class PersonCL {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return 2037 - this.birthYear; // In ES6 Classes we can create the methods directly here, they aren't attached to the objects themselves, but to the prototype.
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+}
+
+const lucas = new PersonCL('Lucas Souza', 2000);
+console.log(lucas.age);
