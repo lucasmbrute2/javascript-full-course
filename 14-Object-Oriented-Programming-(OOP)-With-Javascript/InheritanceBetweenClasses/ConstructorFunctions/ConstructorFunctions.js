e532b4ca7fac1716be0ec0b585237b96132ebdd6
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-06 01:42:38 -0300

    218=>Constructor Functions: Inheretence classes

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ConstructorFunctions/ConstructorFunctions.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ConstructorFunctions/ConstructorFunctions.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear); // Method call is passing the this keyword to the Person function and calling it
+  this.course = course;
+};
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and i study ${this.course}`);
+};
+
+const mike = new Student('Lucas', 2000, 'Computer Science');
+console.log(mike);
+mike.introduce();
