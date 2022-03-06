e4b335540efeb02186ed41ed229d3a0786554e42
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-06 03:12:07 -0300

    221=>Inheretence with Object.create syntax

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ObjectCreate/ObjectCreate.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ObjectCreate/ObjectCreate.js
@@ -6,9 +6,23 @@ const PersonProto = {
   },
   init(firstName, birthYear) {
     this.firstName = firstName;
-    this.birthyear = birthYear;
+    this.birthYear = birthYear;
   },
 };
 
 const steven = Object.create(PersonProto);
-console.log(steven);
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and i study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2000, 'Computer Science');
+jay.introduce();
+jay.calcAge();
