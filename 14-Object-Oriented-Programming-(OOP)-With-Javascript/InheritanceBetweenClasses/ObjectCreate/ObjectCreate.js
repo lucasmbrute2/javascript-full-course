d9e33a275bb0931499f18ab09565d5c6acd56c4e
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-06 02:55:49 -0300

    221=>Fixing import

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ObjectCreate/ObjectCreate.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ObjectCreate/ObjectCreate.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthyear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
