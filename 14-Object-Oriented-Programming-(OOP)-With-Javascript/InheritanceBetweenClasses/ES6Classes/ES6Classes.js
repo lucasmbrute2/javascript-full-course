ee63c41a6551c29689100204f431a6a7639d8c03
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-06 02:48:51 -0300

    220 => ES6 Syntax to inheretence classes in JS

--- 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ES6Classes/ES6Classes.js
+++ 14-Object-Oriented-Programming-(OOP)-With-Javascript/InheritanceBetweenClasses/ES6Classes/ES6Classes.js
@@ -1,1 +1,50 @@
 'use strict';
+class PersonCL {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    return console.log(2037 - this.birthYear);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+class StudentCL extends PersonCL {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  } // If no has addiotanal properties (course in this case) we don't need specify any constructor
+
+  introduce() {
+    console.log(`Hey ${this.fullName}!`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student i feel more like ${
+        2037 - this.birthYear + 10
+      } `
+    );
+  }
+}
+
+const martha = new StudentCL('Martha Jones', 2012, 'Computer Science');
+martha.calcAge();
+martha.introduce();
+martha.calcAge(); // Polymorphism
