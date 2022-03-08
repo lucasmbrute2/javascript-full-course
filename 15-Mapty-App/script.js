358e362afdb64fdd1dae6faa074c6bd960318a45
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-08 18:40:36 -0300

    233 => Leaflet API : global map render

--- 15-Mapty-App/script.js
+++ 15-Mapty-App/script.js
@@ -13,11 +13,22 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 navigator.geolocation?.getCurrentPosition(
   position => {
-    console.log(position);
-
     const { latitude, longitude } = position.coords;
-
     console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
   },
   () => {
     alert('Could not get your position');
