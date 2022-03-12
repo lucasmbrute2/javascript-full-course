5d88b7f433ea6117a07a72933250520795192287
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-12 15:28:33 -0300

    252 =>Consuming Promises

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/promises.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/promises.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// const request = new XMLHttpRequest(); // Old school method
+// request.open('GET', `https://restcountries.com/v3.1/${param}/${country}`);
+// request.send();
+
+const getCountryData = country => {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+};
+
+getCountryData('brasil');
