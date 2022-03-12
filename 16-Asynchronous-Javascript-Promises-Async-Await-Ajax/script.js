a94a352233495cce9cb07b838507cd0aa11b7f21
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-03-12 14:54:26 -0300

    250 =>Ajax call inside another Ajax call (callback hell)

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/script.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/script.js
@@ -4,39 +4,62 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
+const ajaxCall = (country, param = 'name') => {
   const request = new XMLHttpRequest(); // Old school method
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.open('GET', `https://restcountries.com/v3.1/${param}/${country}`);
   request.send();
+  return request;
+};
+
+const renderCountry = data => {
+  const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${
+      data.languages[Object.keys(data.languages)]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      data.currencies[Object.keys(data.currencies)].name
+    }</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX Call country 1
+  const request = ajaxCall(country);
 
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    console.log(data);
+    renderCountry(data);
 
-    const html = `
-      <article class="country">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)}</p>
-        <p class="country__row"><span>🗣️</span>${
-          data.languages[Object.keys(data.languages)]
-        }</p>
-        <p class="country__row"><span>💰</span>${
-          data.currencies[Object.keys(data.currencies)].name
-        }</p>
-      </div>
-    </article>`;
+    // Render country 1
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    // Get neighbour country(2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    // AJAX Call country 2
+    const request2 = ajaxCall(neighbour, 'alpha');
+
+    request2.addEventListener('load', function () {
+      const [data] = JSON.parse(this.responseText);
+      console.log(data);
+      renderCountry(data);
+    });
   });
 };
 
-getCountryData('brasil');
-getCountryData('usa');
-getCountryData('mexico');
-getCountryData('germany');
+getCountryAndNeighbour('brasil');
+getCountryAndNeighbour('usa');
+getCountryAndNeighbour('mexico');
+getCountryAndNeighbour('germany');
