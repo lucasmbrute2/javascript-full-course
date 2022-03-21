12ea18b38e207c048a7656c17b0a5204a3e78941
Lucas <lucasmbrute614@gmail.com>
2022-03-20 22:53:49 -0300

    258=> The Event Loop in Practice: Callback priority

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/EventLoop/EventLoop.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/EventLoop/EventLoop.js
@@ -1,1 +1,13 @@
 'use strict';
+
+console.log('Test start'); // first
+setTimeout(() => console.log('0 sec timer'), 0); // quarto
+Promise.resolve('Resolved promise 1').then(res => console.log(res)); // terceiro
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 10000000; i++) {}
+  console.log(res);
+});
+console.log('Test end'); // second
+
+// Notice that setTimout will be resolved only after the "Resolved promise 2" is done!
