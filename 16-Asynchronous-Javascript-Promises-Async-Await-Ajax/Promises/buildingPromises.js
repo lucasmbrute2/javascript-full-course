a8b7e7b10329345f2ea056f4f31e16e3ebbbd438
Lucas <lucasmbrute614@gmail.com>
2022-03-21 21:28:08 -0300

    259=> Promise builded

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/buildingPromises.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/buildingPromises.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const lotteryPromise = new Promise((resolve, reject) => {
+  if (Math.random() >= 0.5) return resolve('You won!!!!'); // set the Promise as "fullfield"
+
+  reject("I'm sorry, you lose :("); // set the Promise as "rejected"
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
