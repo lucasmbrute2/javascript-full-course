34829e192647d94bf18f314f43ac6d931405f778
Lucas <lucasmbrute614@gmail.com>
2022-03-25 21:51:00 -0300

    Refactoring the previous geolocation function to Async Await

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/Async-Await/AsyncAwait.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/Async-Await/AsyncAwait.js
@@ -1,13 +1,57 @@
 'use strict';
 
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = (data, className = '') => {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${
+      data.languages[Object.keys(data.languages)]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      data.currencies[Object.keys(data.currencies)].name
+    }</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const getGeoLocation = async () => {
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+  const geoCodeResponse = await fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json`
+  );
+  const geoCodeBody = await geoCodeResponse.json();
+  return geoCodeBody;
+};
+
 const whereAmI = async country => {
   // Async keyword make returns a promise automatically
+
+  const geoCodeBody = await getGeoLocation();
+  console.log(geoCodeBody);
 
   const response = await fetch(
     // Allows the await keyword
     `https://restcountries.com/v3.1/name/${country}`
   );
-  const body = await response.json();
-  console.log(response);
+  const [body] = await response.json();
+  renderCountry(body);
 };
 whereAmI('brazil');
