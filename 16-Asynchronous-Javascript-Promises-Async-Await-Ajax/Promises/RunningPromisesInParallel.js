8f204f6cb1d214d472245bdebd599ab6ce633d40
Lucas <lucasmbrute614@gmail.com>
2022-03-26 00:29:43 -0300

    265 A bad way to render multiples calls

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/RunningPromisesInParallel.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/RunningPromisesInParallel.js
@@ -1,3 +1,32 @@
 'use strict';
 
 // Rendering 3 countries data without caring about the sort of arrival in promises.
+
+const getJSON = (url, error = 'Something went wrong') => {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${error} ${response.status}`); // This "throw" imediatly rejects the promise and returns
+
+    return response.json();
+  });
+};
+
+const getCountries = async (...countries) => {
+  const [country1, country2, country3] = countries;
+
+  try {
+    const [response1] = await getJSON(
+      `https://restcountries.com/v3.1/name/${country1}`
+    );
+    const [response2] = await getJSON(
+      `https://restcountries.com/v3.1/name/${country2}`
+    );
+    const [response3] = await getJSON(
+      `https://restcountries.com/v3.1/name/${country3}`
+    );
+
+    console.log(response1.capital, response2.capital, response3.capital);
+  } catch (err) {
+    console.error(err);
+  }
+};
+getCountries('brasil', 'germany', 'argentina');
