55083224bcd094037c418ef53c987edf15508022
Lucas <lucasmbrute614@gmail.com>
2022-03-25 21:39:07 -0300

    262=> How async await works

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/Async-Await/AsyncAwait.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/Async-Await/AsyncAwait.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const whereAmI = async country => {
+  // Async keyword make returns a promise automatically
+
+  const response = await fetch(
+    // Allows the await keyword
+    `https://restcountries.com/v3.1/name/${country}`
+  );
+  const body = await response.json();
+  console.log(response);
+};
+whereAmI('brazil');
