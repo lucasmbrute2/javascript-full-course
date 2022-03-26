8dc7906bbb366234d2278041faa42c23ade3ffb9
Lucas <lucasmbrute614@gmail.com>
2022-03-26 00:50:10 -0300

    265 Promise.All static method

--- 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/RunningPromisesInParallel.js
+++ 16-Asynchronous-Javascript-Promises-Async-Await-Ajax/Promises/RunningPromisesInParallel.js
@@ -13,8 +13,9 @@ const getJSON = (url, error = 'Something went wrong') => {
 const getCountries = async (...countries) => {
   const [country1, country2, country3] = countries;
 
+  // This is a bad way, cuz all the promises will depends of each one other
   try {
-    const [response1] = await getJSON(
+    /*const [response1] = await getJSON(
       `https://restcountries.com/v3.1/name/${country1}`
     );
     const [response2] = await getJSON(
@@ -22,9 +23,16 @@ const getCountries = async (...countries) => {
     );
     const [response3] = await getJSON(
       `https://restcountries.com/v3.1/name/${country3}`
-    );
+    );*/
 
-    console.log(response1.capital, response2.capital, response3.capital);
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${country1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${country2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${country3}`),
+    ]);
+    console.log(data.map(country => country[0].capital[0])); // If one of the promises are reject, the whole promise.all actually rejects as well
+
+    // console.log(response1.capital, response2.capital, response3.capital);
   } catch (err) {
     console.error(err);
   }
