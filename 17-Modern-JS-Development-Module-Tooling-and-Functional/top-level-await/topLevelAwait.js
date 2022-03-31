bf9bd7b699e8ca94d3e0134ab783ac7849259262
Lucas <lucasmbrute614@gmail.com>
2022-03-30 23:15:36 -0300

    273=> How works top level await with response of functions"

--- 17-Modern-JS-Development-Module-Tooling-and-Functional/top-level-await/topLevelAwait.js
+++ 17-Modern-JS-Development-Module-Tooling-and-Functional/top-level-await/topLevelAwait.js
@@ -1,5 +1,15 @@
 const response = await fetch('https://jsonplaceholder.typicode.com/posts');
 const data = await response.json();
 console.log(data);
+console.log('Logged after the await');
+// About modules, we can use await keyword outside of async function, ONLY IN MODULES,but will stop all the module until data is retrivied,becareful!
 
-// About modules, we can use await keyword outside of async function, ONLY IN MODULES
+const getLastPost = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  // console.log(data.slice(-1));
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
