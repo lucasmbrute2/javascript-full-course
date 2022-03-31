96ee7a4373e405d49921fdde8cd5a18eb003bfaa
Lucas <lucasmbrute614@gmail.com>
2022-03-31 13:33:46 -0300

    277=> Deep clone object with loadash

--- 17-Modern-JS-Development-Module-Tooling-and-Functional/NPM/loadash.js
+++ 17-Modern-JS-Development-Module-Tooling-and-Functional/NPM/loadash.js
@@ -1,1 +1,23 @@
-'use strict';
+import cloneDeep from '/node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    {
+      product: 'bread',
+      quantity: 5,
+    },
+    {
+      product: 'pizza',
+      quantity: 5,
+    },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+
+const stateClone = Object.assign({}, state); // It is a shallow copy
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone); // In this clone object is not muted
