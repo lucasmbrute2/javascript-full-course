db86a864d6c9841e9e85bdb913550ceb0fbd720a
Lucas <lucasmbrute614@gmail.com>
2022-03-30 22:29:05 -0300

    272=>  exporting functions

--- 17-Modern-JS-Development-Module-Tooling-and-Functional/script.js
+++ 17-Modern-JS-Development-Module-Tooling-and-Functional/script.js
@@ -1,3 +1,5 @@
 //importing module
-import './shoppingCart.js';
+import { addToCart } from './shoppingCart.js';
 console.log('importing module');
+
+addToCart('bread', 5);
--- 17-Modern-JS-Development-Module-Tooling-and-Functional/shoppingCart.js
+++ 17-Modern-JS-Development-Module-Tooling-and-Functional/shoppingCart.js
@@ -1,2 +1,11 @@
 // exporting module
 console.log('exporting module');
+
+// To export variables we need to export with the keyword
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = (product, quantity) => {
+  cart.push(product, quantity);
+  console.log(`${quantity} ${product} added to cart`);
+};
