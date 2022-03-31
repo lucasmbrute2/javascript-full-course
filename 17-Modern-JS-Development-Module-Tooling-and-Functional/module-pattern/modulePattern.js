986cca0039b9ba5ade3b00f6f87b856ef3670a45
Lucas <lucasmbrute614@gmail.com>
2022-03-30 23:47:51 -0300

    274=>How a Module Pattern works

---
+++ 17-Modern-JS-Development-Module-Tooling-and-Functional/module-pattern/modulePattern.js
@@ -0,0 +1,26 @@
+const ShoppingCart = (() => {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = (product, quantity) => {
+    cart.push(product, quantity);
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = (product, quantity) => {
+    cart.push(product, quantity);
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart.addToCart('apple', 4);
+ShoppingCart.orderStock('apple', 4); // We can't use this orderStock function because was not returned
