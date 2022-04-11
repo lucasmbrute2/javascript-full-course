6d49bd78c7512a8431d5162d6c32452ba53fa712
Lucas <lucasmbrute614@gmail.com>
2022-04-11 09:30:26 -0300

    293=> Exporting variables to all the aplication from config file

--- 18-Forkify-App-Building-Modern-Application/src/js/model.js
+++ 18-Forkify-App-Building-Modern-Application/src/js/model.js
@@ -1,12 +1,12 @@
+import { API_URL } from './config';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async id => {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
+    const res = await fetch(`${API_URL}${id}`);
     const {
       data: { recipe },
     } = await res.json();
