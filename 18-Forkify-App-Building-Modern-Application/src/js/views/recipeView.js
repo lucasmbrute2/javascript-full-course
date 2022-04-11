007e7d95f063c928235b81732f88e4bd0d3e00b8
Lucas <lucasmbrute614@gmail.com>
2022-04-10 22:48:19 -0300

    292=> Fractional library to improve the ingredients description

--- 18-Forkify-App-Building-Modern-Application/src/js/views/recipeView.js
+++ 18-Forkify-App-Building-Modern-Application/src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from '../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -8,6 +9,7 @@ class RecipeView {
     // Receives the data to be rendered
     this.#data = data;
     this.#clear();
+    s;
     this.#parentElement.insertAdjacentHTML(
       'afterbegin',
       this.#generateMarkup()
@@ -94,7 +96,11 @@ class RecipeView {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ingredient.quantity}</div>
+            <div class="recipe__quantity">${
+              ingredient.quantity
+                ? new Fraction(ingredient.quantity).toString()
+                : ''
+            }</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ingredient.unit}</span>
               ${ingredient.description}
