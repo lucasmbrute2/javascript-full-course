d9b8f29d8104e81dd73e05282c14495253cbac02
Lucas <lucasmbrute614@gmail.com>
2022-04-10 22:53:52 -0300

    293=> Refactoring callback map of ingredients to a private method

--- 18-Forkify-App-Building-Modern-Application/src/js/views/recipeView.js
+++ 18-Forkify-App-Building-Modern-Application/src/js/views/recipeView.js
@@ -89,26 +89,7 @@ class RecipeView {
   <div class="recipe__ingredients">
   <h2 class="heading--2">Recipe ingredients</h2>
   <ul class="recipe__ingredient-list">
-    ${this.#data.ingredients
-      .map(ingredient => {
-        return `
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="${icons}#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">${
-              ingredient.quantity
-                ? new Fraction(ingredient.quantity).toString()
-                : ''
-            }</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">${ingredient.unit}</span>
-              ${ingredient.description}
-            </div>
-          </li>
-          `;
-      })
-      .join('')}
+    ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
         
         </ul>
       </div>
@@ -132,6 +113,25 @@ class RecipeView {
       </svg>
     </a>
   </div>`;
+  }
+
+  #generateMarkupIngredient(ingredient) {
+    return `
+        <li class="recipe__ingredient">
+          <svg class="recipe__icon">
+            <use href="${icons}#icon-check"></use>
+          </svg>
+          <div class="recipe__quantity">${
+            ingredient.quantity
+              ? new Fraction(ingredient.quantity).toString()
+              : ''
+          }</div>
+          <div class="recipe__description">
+            <span class="recipe__unit">${ingredient.unit}</span>
+            ${ingredient.description}
+          </div>
+        </li>
+        `;
   }
 }
 
