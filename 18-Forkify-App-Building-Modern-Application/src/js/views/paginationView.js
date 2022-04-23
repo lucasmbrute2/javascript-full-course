0a010560095642c4f1e496769664a3d87db6133f
Lucas <lucasmbrute614@gmail.com>
2022-04-23 09:30:17 -0300

    299=> Fixing if condition to render results in right page

--- 18-Forkify-App-Building-Modern-Application/src/js/views/paginationView.js
+++ 18-Forkify-App-Building-Modern-Application/src/js/views/paginationView.js
@@ -9,16 +9,18 @@ class PaginationView extends View {
       this._data.results.length / this._data.resultsPerPage
     );
 
+    let html = 'Only 1';
     // Page1, and there are other pages
-    if (this._data.page === 1 && numPages > 1) return 'page 1, others';
+    if (this._data.page === 1 && numPages > 1) html = 'page 1, others';
 
     //Last page
-    if (this._data.page === numPages && numPages > 1) return 'last page';
+    if (this._data.page === numPages && numPages > 1) html = 'last page';
 
     //Other page
-    if (this._data.page < numPages) return 'other page';
+    if (this._data.page != 1 && this._data.page < numPages) html = 'other page';
     // Page1, and there are NO other pages
-    return 'Only 1';
+    console.log(html);
+    return html;
   }
 }
 
